7d0844bdacf98f7bd50ab6edbac3b2f89900d1bf
Mawdac <edwardreillycomputers@gmail.com>
2024-01-03 08:47:49 -0500

    chore: hide all dotfiles initially

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -4,11 +4,12 @@ local minifiles_toggle = function(...)
   end
 end
 
-local filter_show = function()
+local show_dotfiles = false
+local filter_show = function(fs_entry)
   return true
 end
 local filter_hide = function(fs_entry)
-  return not vim.startswith(fs_entry.name, ".git")
+  return not vim.startswith(fs_entry.name, ".")
 end
 
 return {
@@ -32,15 +33,17 @@ return {
   config = function(_, opts)
     require("mini.files").setup(opts)
 
-    local show_dotfiles = function()
-      require("mini.files").refresh({ content = { filter = filter_show } })
+    local toggle_dotfiles = function()
+      show_dotfiles = not show_dotfiles
+      local new_filter = show_dotfiles and filter_show or filter_hide
+      require("mini.files").refresh({ content = { filter = new_filter } })
     end
 
     vim.api.nvim_create_autocmd("User", {
       pattern = "MiniFilesBufferCreate",
       callback = function(args)
         local buf_id = args.data.buf_id
-        vim.keymap.set("n", "g.", show_dotfiles, { buffer = buf_id })
+        vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
       end,
     })
 
