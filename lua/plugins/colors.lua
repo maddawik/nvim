589b6340a2050ddbbc95cc58deafa1a1c673cf55
Mawdac <edwardreillycomputers@gmail.com>
2024-01-05 16:50:58 -0500

    style: add solarized-osaka

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -2,6 +2,12 @@ return {
   { "rebelot/kanagawa.nvim", event = "VeryLazy" },
   { "folke/tokyonight.nvim", event = "VeryLazy" },
   {
+    "craftzdog/solarized-osaka.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    opts = { transparent = false },
+  },
+  {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
