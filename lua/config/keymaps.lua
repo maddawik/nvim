34dea0b158db9b6b406db9bbf19150754ac24da9
Mawdac <edwardreillycomputers@gmail.com>
2024-01-05 16:51:23 -0500

    feat: make conceallevel toggle move through 4 levels

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,3 +16,8 @@ keymap.set("n", "<C-Up>", "<cmd>SmartResizeUp<CR>")
 keymap.set("n", "<C-Down>", "<cmd>SmartResizeDown<CR>")
 keymap.set("n", "<C-Left>", "<cmd>SmartResizeLeft<CR>")
 keymap.set("n", "<C-Right>", "<cmd>SmartResizeRight<CR>")
+
+keymap.set("n", "<leader>uc", function()
+  local next_state = vim.o.conceallevel > 0 and vim.o.conceallevel - 1 or 3
+  require("lazyvim.util").toggle("conceallevel", false, { vim.o.conceallevel, next_state % 4 })
+end, { desc = "Toggle Conceal" })
