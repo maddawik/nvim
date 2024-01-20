241c549f1fcf5eefb34920dea357542c56bb4c49
Mawdac <edwardreillycomputers@gmail.com>
2024-01-19 22:52:08 -0500

    chore: add keys for pull/push and update cmds

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,8 +1,10 @@
 return {
   "tpope/vim-fugitive",
-  cmd = "Git",
+  cmd = { "Git", "G", "GBrowse" },
   keys = {
-    { "<leader>gs", "<Cmd>vertical Git<CR>", desc = "Fugitive" },
+    { "<leader>gf", "<Cmd>vertical G<CR>", desc = "Fugitive" },
+    { "<leader>gp", "<Cmd>G pull<CR>", desc = "pull" },
+    { "<leader>gP", "<Cmd>G push<CR>", desc = "push" },
   },
   dependencies = {
     "tpope/vim-rhubarb",
