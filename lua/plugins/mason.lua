ed4bd3687b852e99aa2fd47baac2d26bf3ba7e81
Mawdac <edwardreillycomputers@gmail.com>
2024-01-28 20:04:23 -0500

    style: update mason ui

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,15 @@
 return {
   "williamboman/mason.nvim",
   opts = {
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗",
+      },
+      border = "rounded",
+      height = 0.8,
+    },
     ensure_installed = {
       "shellcheck",
     },
