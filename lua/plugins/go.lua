5b8034d5db331b00aeddf5b8de50cd2195cf7742
Mawdac <edwardreillycomputers@gmail.com>
2024-02-26 11:55:50 -0500

    feat: switch gopher.nvim for go.nvim

---
+++ lua/plugins/go.lua
@@ -0,0 +1,22 @@
+return {
+  -- "ray-x/go.nvim",
+  "Mawdac/go.nvim",
+  branch = "fix/inlay-hints",
+  dependencies = { -- optional packages
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = true,
+  event = { "CmdlineEnter" },
+  ft = { "go", "gomod" },
+  build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  keys = {
+    {
+      "<leader>ce",
+      "<cmd>GoIfErr<cr>",
+      desc = "Go Error",
+      ft = "go",
+    },
+  },
+}
