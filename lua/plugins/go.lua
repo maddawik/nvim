a833d1f376b4618b1eafc0a8b34d31444d9f4b1d
Mawdac <edwardreillycomputers@gmail.com>
2024-03-08 12:53:43 -0500

    fix: add autocmd for inlay-hints on nightly

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,13 +1,15 @@
 return {
-  -- "ray-x/go.nvim",
-  "Mawdac/go.nvim",
-  branch = "fix/inlay-hints",
-  dependencies = { -- optional packages
+  "ray-x/go.nvim",
+  dependencies = {
     "ray-x/guihua.lua",
     "neovim/nvim-lspconfig",
     "nvim-treesitter/nvim-treesitter",
   },
-  config = true,
+  opts = {
+    lsp_inlay_hints = {
+      enable = false, -- autocmd for enabling
+    },
+  },
   event = { "CmdlineEnter" },
   ft = { "go", "gomod" },
   -- build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
