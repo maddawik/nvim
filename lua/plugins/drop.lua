38d05e53eb112c4d968d9b449df68f40c97b0644
Mawdac <edwardreillycomputers@gmail.com>
2024-03-11 13:14:01 -0400

    style: update screensaver

--- lua/plugins/drop.lua
+++ lua/plugins/drop.lua
@@ -1,36 +1,42 @@
-function GetCurrentSeason(month)
-  math.randomseed(os.time())
-  local n = math.random()
-  if n > 0.98 then
-    return "stars"
+local function pickTheme()
+  -- Check if we are in a work directory
+  if string.match(vim.fn.getcwd() or "", "work") then
+    return { symbols = { "🦊", "🔒", "💻" }, colors = {} }
   end
-  if month == 12 then
-    if n > 0.3 then
-      return "xmas"
-    else
-      return "snow"
-    end
-  elseif month == 1 or month == 2 then
-    return "snow"
-  elseif month >= 3 and month <= 5 then
-    return "spring"
-  elseif month >= 6 and month == 8 then
-    return "summer"
-  elseif month >= 9 and month <= 11 then
-    return "leaves"
+
+  -- Small chance of stars
+  math.randomseed(os.time())
+  if math.random() > 0.98 then
+    return "stars"
+  else
+    return { symbols = { "💻", "👱", " " }, colors = { "#00ADD8" } }
   end
 end
 
 return {
-  "folke/drop.nvim",
-  event = "VeryLazy",
-  enabled = true,
-  config = function()
-    require("drop").setup({
-      filetypes = {},
-      winblend = 50,
-      screensaver = 1000 * 60 * 60, -- 1 hour
-      theme = GetCurrentSeason(os.date("*t", os.time()).month),
-    })
-  end,
+  {
+    "catdadcode/drop.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      local thanksgiving_day = require("drop").calculate_us_thanksgiving(os.date("%Y"))
+      opts.theme = pickTheme()
+      opts.holidays = {
+        halloween = {
+          start_date = { month = 10, day = 1 },
+          end_date = { month = 10, day = 31 },
+        },
+        us_thanksgiving = {
+          start_date = { month = 11, day = 10 },
+          end_date = thanksgiving_day,
+        },
+        xmas = {
+          start_date = { month = 12, day = 1 },
+          end_date = { month = 12, day = 25 },
+        },
+      }
+      opts.winblend = 50
+      opts.filetypes = {}
+      opts.screensaver = 1000 * 60 * 30
+    end,
+  },
 }
