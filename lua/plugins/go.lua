3efff92ef50796e9a75d0e58b9d5d2c395445eac
Mawdac <edwardreillycomputers@gmail.com>
2024-03-15 16:32:33 -0400

    feat: add revive to go config

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,24 +1,42 @@
 return {
-  "ray-x/go.nvim",
-  dependencies = {
-    "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  opts = {
-    lsp_inlay_hints = {
-      enable = false, -- autocmd for enabling
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        go = { "revive" },
+      },
     },
   },
-  event = { "CmdlineEnter" },
-  ft = { "go", "gomod" },
-  -- build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-  keys = {
-    {
-      "<leader>ce",
-      "<cmd>GoIfErr<cr>",
-      desc = "Go Error",
-      ft = "go",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "revive",
+      },
+    },
+  },
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      lsp_inlay_hints = {
+        enable = false, -- autocmd for enabling
+      },
+    },
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    -- build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    keys = {
+      {
+        "<leader>ce",
+        "<cmd>GoIfErr<cr>",
+        desc = "Go Error",
+        ft = "go",
+      },
     },
   },
 }
