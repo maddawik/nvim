0a689a626ceb1df3217710be142df8ba06bf5313
Mawdac <edwardreillycomputers@gmail.com>
2024-05-11 13:33:09 -0400

    style(options): show eol character

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,14 +1,14 @@
 vim.opt.colorcolumn = "80"
 vim.opt.pumblend = 0
--- vim.opt.listchars = {
---   space = ".",
---   eol = "↲",
---   nbsp = "␣",
---   trail = "·",
---   precedes = "←",
---   extends = "→",
---   tab = "¬ ",
---   conceal = "※",
--- }
+vim.opt.listchars = {
+  --   space = ".",
+  eol = "↲",
+  --   nbsp = "␣",
+  --   trail = "·",
+  --   precedes = "←",
+  --   extends = "→",
+  --   tab = "¬ ",
+  --   conceal = "※",
+}
 
 vim.g.loaded_perl_provider = 0
