e1e05573c9915f2eb000658adbdad5b2d1b5ecd0
Mawdac <edwardreillycomputers@gmail.com>
2024-05-15 17:59:28 -0400

    feat(git): add linker for to replace fugitive's GBrowse

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -46,4 +46,14 @@ return {
     config = true,
     cmd = "Octo",
   },
+  -- Web Links
+  {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    opts = {},
+    keys = {
+      { "<leader>gy", "<cmd>GitLink<cr>",  mode = { "n", "v" }, desc = "Yank git link" },
+      { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+    },
+  },
 }
