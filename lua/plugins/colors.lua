551a431bf3e7862ed1ae008f9460163525cdad15
Mawdac <edwardreillycomputers@gmail.com>
2024-05-19 21:12:19 -0400

    chore: bump deps

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -5,9 +5,6 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    -- opts = {
-    --   transparent_background = true,
-    -- },
   },
   {
     "LazyVim/LazyVim",
