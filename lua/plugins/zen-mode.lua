4008cdee7bc8f24ffb9a1ecd13aae7e24eff395d
Mawdac <edwardreillycomputers@gmail.com>
2024-05-24 10:25:19 -0400

    feat(zen-mode): add center buffer mode

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -2,11 +2,39 @@ return {
   "folke/zen-mode.nvim",
   dependencies = { "folke/twilight.nvim" },
   cmd = "ZenMode",
-  keys = { { "<leader>uz", "<cmd>ZenMode<cr>", desc = "Toggle Zen-Mode" } },
-  opts = {
-    plugins = {
-      options = { laststatus = 0 },
-      tmux = { enabled = true },
+  keys = {
+    {
+      "<leader>uz",
+      function()
+        require("zen-mode").toggle({
+          plugins = {
+            options = { laststatus = 3 },
+            tmux = { enabled = false },
+            twilight = { enabled = false },
+          },
+        })
+      end,
+      desc = "Zen-Mode Center",
+    },
+    {
+      "<leader>uZ",
+      function()
+        require("zen-mode").toggle({
+          plugins = {
+            options = { laststatus = 0 },
+            tmux = { enabled = true },
+            twilight = { enabled = true },
+          },
+          on_open = function()
+            require("incline").disable()
+          end,
+          on_close = function()
+            require("incline").enable()
+          end,
+        })
+      end,
+      desc = "Zen-Mode Focus",
     },
   },
+  opts = {},
 }
