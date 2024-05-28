3a23772cace886ed01d378f59aafe3e06b7d5b75
Mawdac <edwardreillycomputers@gmail.com>
2024-05-28 08:43:20 -0400

    feat: start migrating to mini-hipatterns

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -38,6 +38,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     { import = "lazyvim.plugins.extras.util.project" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 
     -- NOTE: This should always be last
     { import = "plugins" },
