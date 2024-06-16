3af4a257683a20f570ee8e1bc1fc6a05be3abdf1
Mawdac <edwardreillycomputers@gmail.com>
2024-06-16 11:29:42 -0400

    style(which-key): no border

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,9 +3,9 @@ return {
   opts = {
     layout = { align = "center" },
     show_help = false,
-    window = {
-      -- winblend = 10,
-      border = "rounded", -- none, single, double, shadow
-    },
+    -- window = {
+    --   winblend = 10,
+    --   border = "rounded", -- none, single, double, shadow
+    -- },
   },
 }
