4d9bdb38ff7dfa19f05fbbd66cd5ed3f5eb26b64
maddawik <edwardreillycomputers@gmail.com>
2024-07-13 12:51:02 -0400

    chore: remove go.nvim and bump deps

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -15,28 +15,4 @@ return {
       },
     },
   },
-  {
-    "ray-x/go.nvim",
-    dependencies = {
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    opts = {
-      lsp_inlay_hints = {
-        enable = false, -- better managed without this
-      },
-    },
-    event = { "CmdlineEnter" },
-    ft = { "go", "gomod" },
-    -- build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-    keys = {
-      {
-        "<leader>ce",
-        "<cmd>GoIfErr<cr>",
-        desc = "Go Error",
-        ft = "go",
-      },
-    },
-  },
 }
