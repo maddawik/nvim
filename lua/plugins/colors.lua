fb39df06f299bc385d78973b9395b2909f3596f6
maddawik <edwardreillycomputers@gmail.com>
2024-07-23 20:16:08 -0400

    fix: use catppuccin grug-far integration

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -5,6 +5,11 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    opts = {
+      integrations = {
+        grug_far = true,
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
