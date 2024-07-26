da3dba6a74dc7b3674bb09fbfd94c0a8f35220cc
maddawik <edwardreillycomputers@gmail.com>
2024-07-26 17:04:47 -0400

    feat(go): add gopher.nvim

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -15,4 +15,21 @@ return {
       },
     },
   },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "mfussenegger/nvim-dap",
+    },
+    keys = {
+      { "<leader>ce", "<cmd>GoIfErr<cr>", ft = "go", desc = "Go Error Snippet" },
+    },
+    -- (optional) will update plugin's deps on every update
+    -- build = function()
+    --   vim.cmd("GoInstallDeps")
+    -- end,
+    opts = {},
+  },
 }
