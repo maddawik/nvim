0490adcbefc018e8f78aae2bb14c2c9755385d97
maddawik <edwardreillycomputers@gmail.com>
2024-07-27 11:09:50 -0400

    chore(gitsigns): remove

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,33 +1,4 @@
 return {
-  -- Visuals
-  {
-    "lewis6991/gitsigns.nvim",
-    enabled = true, -- mini-diff extra disables this by default
-    opts = {
-      current_line_blame = true,
-      -- disable all keymaps, only use current_line_blame functinoality
-      on_attach = function() end,
-    },
-    keys = {
-      {
-        "<leader>gh",
-        function()
-          LazyVim.lazygit.browse()
-        end,
-        mode = { "n", "v" },
-        desc = "Git Browse",
-      },
-      {
-        "<leader>gB",
-        function()
-          require("gitsigns").blame()
-        end,
-        mode = { "n", "v" },
-        desc = "Git Blame",
-      },
-    },
-  },
-
   -- Web Links
   {
     "linrongbin16/gitlinker.nvim",
