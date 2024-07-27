ca6006036acfcfd39c8567f4d51ffdb278a75f7d
maddawik <edwardreillycomputers@gmail.com>
2024-07-27 11:43:48 -0400

    feat(dashboard): add quote to footer sometimes

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,3 +1,20 @@
+local function pickQuote()
+  -- Small chance of encouragement from Master Miller
+  math.randomseed(os.time())
+  if math.random() > 0.98 then
+    return {
+      "",
+      "\"Sometimes in combat or when you're near the end of your rope,",
+      "You can see things that normally aren't there or shouldn't be there.",
+      "Relax. It's not a bug. It's just the mysteries of the human mind.\"",
+      "",
+      "• Master Miller",
+    }
+  else
+    return {}
+  end
+end
+
 return {
   "nvimdev/dashboard-nvim",
   opts = {
@@ -13,7 +30,7 @@ return {
         limit = 6,
         cwd_only = true,
       },
-      footer = {},
+      footer = pickQuote(),
       shortcut = {
         {
           desc = "lazy",
