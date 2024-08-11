381fba26a1661572dfa611b5e22f223fc4fac541
maddawik <edwardreillycomputers@gmail.com>
2024-08-11 15:17:54 -0400

    fix: enable fzf integration in catppuccin, bump deps

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -5,6 +5,11 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    opts = {
+      integrations = {
+        fzf = true,
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,10 +1,3 @@
-local hl = vim.api.nvim_set_hl
-hl(0, "FzfLuaBorder", { link = "FloatBorder" })
-hl(0, "FzfLuaHeaderText", { link = "Function" })
-hl(0, "FzfLuaLiveSym", { link = "Error" })
-hl(0, "FzfLuaBufFlagCur", { link = "String" })
-hl(0, "FzfLuaPathLineNr", { link = "Exception" })
-
 return {
   "ibhagwan/fzf-lua",
   keys = {
