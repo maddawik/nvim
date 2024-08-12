5091b1f0390dbca8b9a91450aa00ec571af3f44b
maddawik <edwardreillycomputers@gmail.com>
2024-08-12 13:08:24 -0400

    feat: add cmp-cmdline

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,14 +1,47 @@
 return {
   {
     "mtoohey31/cmp-fish",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
     ft = "fish",
   },
   {
     "andersevenrud/cmp-tmux",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
     event = {
       "BufReadPre",
       "BufNewFile",
     },
+  },
+  {
+    "hrsh7th/cmp-cmdline",
+    dependencies = { "hrsh7th/nvim-cmp", "hrsh7th/cmp-buffer" },
+    event = "CmdlineEnter",
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" },
+            },
+          },
+        }),
+      })
+    end,
   },
   {
     "hrsh7th/nvim-cmp",
