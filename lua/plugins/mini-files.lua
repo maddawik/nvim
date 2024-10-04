ea55982a0de0885cca9604d6a8b71cdd41edc85a
maddawik <edwardreillycomputers@gmail.com>
2024-10-04 17:38:40 -0400

    feat: add grug-far integration with mini-files

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -6,14 +6,42 @@ return {
   {
     "echasnovski/mini.files",
     lazy = false,
-    opts = {
-      mappings = {
+    opts = function(_, opts)
+      opts.mappings = {
         synchronize = "<CR>",
-      },
-      options = {
+      }
+      opts.options = {
         use_as_default_explorer = true,
-      },
-    },
+      }
+      local files_grug_far_replace = function(path)
+        local MiniFiles = require("mini.files")
+        -- works only if cursor is on the valid file system entry
+        local cur_entry_path = MiniFiles.get_fs_entry().path
+        local prefills = { paths = vim.fs.dirname(cur_entry_path) }
+
+        local grug_far = require("grug-far")
+
+        -- instance check
+        if not grug_far.has_instance("explorer") then
+          grug_far.open({
+            instanceName = "explorer",
+            prefills = prefills,
+            staticTitle = "Find and Replace from Explorer",
+          })
+        else
+          grug_far.open_instance("explorer")
+          -- updating the prefills without crealing the search and other fields
+          grug_far.update_instance_prefills("explorer", prefills, false)
+        end
+      end
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesBufferCreate",
+        callback = function(args)
+          vim.keymap.set("n", "gs", files_grug_far_replace, { buffer = args.data.buf_id, desc = "Search in directory" })
+        end,
+      })
+    end,
     -- Passing a function that returns the table of keymaps overrides any of the
     -- defaults that LazyVim has (<leader>fm, and <leader>fM in this case)
     keys = function()
