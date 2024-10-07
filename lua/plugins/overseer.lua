0e19e1508793fd7559366c2c40df8e0c1100d1fe
maddawik <edwardreillycomputers@gmail.com>
2024-10-07 17:43:06 -0400

    fix(overseer): remove lualine integration, bump deps

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,23 +1,16 @@
 return {
   {
     "stevearc/overseer.nvim",
-  -- stylua: ignore
-  keys = function ()
-    return {
-      { "<leader>oa", "<cmd>OverseerTaskAction<cr>",  desc = "Task Action" },
-      { "<leader>oc", "<cmd>OverseerClearCache<cr>",  desc = "Clear cache" },
-      { "<leader>oi", "<cmd>OverseerInfo<cr>",        desc = "Overseer Info" },
-      { "<leader>on", "<cmd>OverseerBuild<cr>",       desc = "New Task" },
-      { "<leader>oo", "<cmd>OverseerToggle<cr>",      desc = "Task List" },
-      { "<leader>oq", "<cmd>OverseerQuickAction<cr>", desc = "Quick Action" },
-      { "<leader>or", "<cmd>OverseerRun<cr>",         desc = "Run Task" },
-    }
-  end,
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, { "overseer" })
+    keys = function()
+      return {
+        { "<leader>oa", "<cmd>OverseerTaskAction<cr>", desc = "Task Action" },
+        { "<leader>oc", "<cmd>OverseerClearCache<cr>", desc = "Clear cache" },
+        { "<leader>oi", "<cmd>OverseerInfo<cr>", desc = "Overseer Info" },
+        { "<leader>on", "<cmd>OverseerBuild<cr>", desc = "New Task" },
+        { "<leader>oo", "<cmd>OverseerToggle<cr>", desc = "Task List" },
+        { "<leader>oq", "<cmd>OverseerQuickAction<cr>", desc = "Quick Action" },
+        { "<leader>or", "<cmd>OverseerRun<cr>", desc = "Run Task" },
+      }
     end,
   },
 }
