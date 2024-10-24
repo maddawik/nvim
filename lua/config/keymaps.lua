77ab7913033e9d32107e0ba46e1789b9965688a1
maddawik <edwardreillycomputers@gmail.com>
2024-10-24 11:32:47 -0400

    feat: add gh-dash key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,7 @@ vim.keymap.set("i", "jk", "<ESC>")
 
 -- Backspace into insert, helpful for snippets
 vim.keymap.set("s", [[<BS>]], [[<BS>i]])
+
+vim.keymap.set("n", "<Space>gd", function()
+  LazyVim.terminal({ "gh", "dash" })
+end, { desc = "GitHub Dashboard" })
