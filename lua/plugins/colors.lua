febb0840752f6bb72e269c8354c73e385feb2df8
maddawik <edwardreillycomputers@gmail.com>
2024-11-09 20:43:22 -0500

    style(colors): add borderless telescope highlights

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,6 +1,35 @@
 return {
-  { "folke/tokyonight.nvim" },
-  { "catppuccin/nvim", name = "catppuccin" },
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require("tokyonight").setup({
+        -- Borderless Telescope
+        on_highlights = function(hl, c)
+          hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg_dark }
+          hl.TelescopeBorder = { bg = c.bg_dark, fg = c.bg_dark }
+
+          hl.TelescopePromptNormal = { bg = c.bg, fg = c.fg_dark }
+          hl.TelescopePromptBorder = { bg = c.bg, fg = c.bg }
+
+          hl.TelescopePromptTitle = { bg = c.blue, fg = c.bg_dark }
+          hl.TelescopePreviewTitle = { bg = c.green, fg = c.bg_dark }
+          hl.TelescopeResultsTitle = { bg = c.red, fg = c.bg_dark }
+        end,
+      })
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        telescope = {
+          style = "nvchad", -- Borderless
+        },
+      },
+    },
+  },
   {
     "rose-pine/neovim",
     name = "rose-pine",
