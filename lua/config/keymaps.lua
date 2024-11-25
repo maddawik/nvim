6cc9503859ad0a89f56255ae208cd610eb93d576
maddawik <edwardreillycomputers@gmail.com>
2024-11-24 21:11:09 -0500

    fix: add snippet keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,9 @@ vim.keymap.set("i", "jk", "<ESC>")
 -- Backspace into insert, helpful for snippets
 vim.keymap.set("s", [[<BS>]], [[<BS>i]])
 
-vim.keymap.set("n", "<Space>gd", function()
-  Snacks.terminal({ "gh", "dash" })
-end, { desc = "GitHub Dashboard" })
+vim.keymap.set({ "i", "s" }, "<C-J>", function()
+  return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<C-J>"
+end, { expr = true, desc = "Jump Next" })
+vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+  return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<S-Tab>"
+end, { expr = true, desc = "Jump Previous" })
