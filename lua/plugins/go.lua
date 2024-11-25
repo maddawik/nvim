ed28201b974ab86b2c19387c0db8a7a3bb30d6ba
maddawik <edwardreillycomputers@gmail.com>
2024-11-24 21:27:09 -0500

    feat(go): add gopher.nvim back with added tools in mason

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -12,7 +12,34 @@ return {
     opts = {
       ensure_installed = {
         "revive",
+        "iferr",
+        "gomodifytags",
+        "gotests",
+        "impl",
       },
     },
+  },
+  {
+    "olexsmir/gopher.nvim",
+    enabled = function()
+      return LazyVim.has_extra("lang.go")
+    end,
+    ft = "go",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "mfussenegger/nvim-dap",
+    },
+    keys = {
+      { "<leader>ce", "<cmd>GoIfErr<cr>", ft = "go", desc = "Go Error Snippet" },
+    },
+    -- (optional) will update plugin's deps on every update
+    -- build = function()
+    --   vim.cmd("GoInstallDeps")
+    -- end,
+
+    ---@module 'gopher'
+    ---@type gopher.Config
+    opts = {},
   },
 }
