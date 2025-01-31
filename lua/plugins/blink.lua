90bc199f2f297b740a946fdd9b40da7a04162c01
maddawik <edwardreillycomputers@gmail.com>
2025-01-31 10:37:46 -0500

    feat: enable cmdline completion (copied LazyVim config)

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,42 +3,73 @@ return {
   dependencies = {
     "mtoohey31/cmp-fish",
   },
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    sources = {
-      default = { "fish" },
-      providers = {
-        fish = {
-          name = "fish",
-          module = "blink.compat.source",
+  opts = function(_, opts)
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      signature = {
+        enabled = true,
+      },
+      snippets = {
+        expand = function(snippet, _)
+          return LazyVim.cmp.expand(snippet)
+        end,
+      },
+      appearance = {
+        use_nvim_cmp_as_default = false,
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        accept = {
+          auto_brackets = {
+            enabled = true,
+          },
+        },
+        menu = {
+          draw = {
+            treesitter = { "lsp" },
+          },
+        },
+        documentation = {
+          window = {
+            border = "rounded",
+          },
+          auto_show = true,
+          auto_show_delay_ms = 200,
+        },
+        ghost_text = {
+          enabled = vim.g.ai_cmp,
         },
       },
-    },
-    completion = {
-      documentation = {
-        window = {
-          border = "rounded",
+
+      sources = {
+        compat = {},
+        default = { "lsp", "fish", "path", "snippets", "buffer" },
+        providers = {
+          fish = {
+            name = "fish",
+            module = "blink.compat.source",
+          },
         },
       },
-    },
-    signature = {
-      enabled = true,
-    },
-    keymap = {
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<C-e>"] = { "hide" },
-      ["<C-y>"] = { "select_and_accept" },
-      ["<CR>"] = { "fallback" },
 
-      ["<C-p>"] = { "select_prev", "fallback" },
-      ["<C-n>"] = { "select_next", "fallback" },
+      keymap = {
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide" },
+        ["<C-y>"] = { "select_and_accept" },
+        ["<CR>"] = { "fallback" },
 
-      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback" },
 
-      ["<C-j>"] = { "snippet_forward", "fallback" },
-      ["<C-k>"] = { "snippet_backward", "fallback" },
-    },
-  },
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+        ["<C-j>"] = { "snippet_forward", "fallback" },
+        ["<C-k>"] = { "snippet_backward", "fallback" },
+      },
+    }
+
+    return opts
+  end,
 }
