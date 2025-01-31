b509c121e52a91e9d3ab825c8aae6ebc26028da7
maddawik <edwardreillycomputers@gmail.com>
2025-01-31 11:38:16 -0500

    chore: too much noise for most current projects, use pyright

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,4 +9,4 @@ vim.g.loaded_node_provider = 0
 vim.g.snacks_animate = false
 vim.g.lazyvim_picker = "snacks"
 vim.g.lazyvim_cmp = "blink.cmp"
-vim.g.lazyvim_python_lsp = "basedpyright"
+-- vim.g.lazyvim_python_lsp = "basedpyright"
