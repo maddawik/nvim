8e25ae735e1414408090fb61135cbbf42a6d54c3
maddawik <edwardreillycomputers@gmail.com>
2025-09-11 16:56:09 -0400

    chore(chezmoi): add condition for loading

--- lua/plugins/chezmoi.lua
+++ lua/plugins/chezmoi.lua
@@ -3,6 +3,9 @@ return {
     -- highlighting for chezmoi files template files
     "alker0/chezmoi.vim",
     lazy = false,
+    cond = function()
+      return vim.fn.executable("chezmoi") == 1
+    end,
     init = function()
       vim.g["chezmoi#use_tmp_buffer"] = 1
       vim.g["chezmoi#source_dir_path"] = vim.fn.expand("~") .. "/.local/share/chezmoi"
