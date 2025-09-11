300769267922b6176cfe3556452bfec1e7a2b9e6
maddawik <edwardreillycomputers@gmail.com>
2025-09-11 16:56:30 -0400

    feat: CopilotChat.nvim -> claudecode.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -55,20 +55,36 @@ return {
     opts = { use_default_keymaps = false, max_join_length = 150 },
   },
 
-  -- Copilot Chat
+  -- Claude code
   {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    optional = true,
-    opts = {
-      model = "gpt-4.1",
-      mappings = {
-        reset = {
-          normal = "<C-x>",
-          insert = "<C-x>",
-        },
-        accept_diff = {
-          normal = "<C-a>",
-          insert = "<C-a>",
+    "coder/claudecode.nvim",
+    dependencies = { "folke/snacks.nvim" },
+    config = true,
+    keys = {
+      { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+      { "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
+      { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+      { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+      {
+        "<leader>as",
+        "<cmd>ClaudeCodeTreeAdd<cr>",
+        desc = "Add file",
+        ft = "minifiles",
+      },
+      -- Diff management
+      { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+      { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+    },
+
+    specs = {
+      {
+        "folke/which-key.nvim",
+        ---@module 'which-key'
+        ---@type wk.Opts
+        opts = {
+          spec = {
+            { "<leader>a", group = "ai", icon = " ", mode = { "v", "n" } },
+          },
         },
       },
     },
