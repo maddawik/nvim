2a224cac53c27946f8a58da48e00bfcefafb7968
maddawik <edwardreillycomputers@gmail.com>
2025-09-17 21:58:40 -0400

    chore(go): inline var and bump deps

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,5 +1,3 @@
-local lzv = require("lazyvim.util")
-
 return {
   {
     "mfussenegger/nvim-lint",
@@ -24,7 +22,7 @@ return {
   {
     "olexsmir/gopher.nvim",
     enabled = function()
-      return lzv.has_extra("lang.go")
+      return require("lazyvim.util").has_extra("lang.go")
     end,
     cond = function()
       return vim.fn.executable("go") == 1
@@ -33,10 +31,6 @@ return {
     keys = {
       { "<leader>ce", "<cmd>GoIfErr<cr>", ft = "go", desc = "Go Error Snippet" },
     },
-    -- (optional) will update plugin's deps on every update
-    -- build = function()
-    --   vim.cmd("GoInstallDeps")
-    -- end,
 
     ---@module 'gopher'
     ---@type gopher.Config
